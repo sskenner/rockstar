9362ec5103e89b662d4c99ef88f7180f6a0de21e
skA <sskenner@gmail.com>
2019-11-14 09:44:21 -0500

    add login page ui test p1

--- pages/internet.page.js
+++ pages/internet.page.js
@@ -1,6 +1,13 @@
 class Internet {
   get logoImage() { return $('.logo-w img') }
   get authHeader() { return $('h4.auth-header')}
-  
+  get formLabelUsername() { return $('.form-group:nth-child(1) label')}
+  get formLabelPassword() { return $('.form-group:nth-child(2) label')}
+  get username() {
+    return $("#username");
+  }
+  get password() {
+    return $("#password");
+  }
 }
 module.exports = new Internet()
--- test/TraditionalTests.js
+++ test/TraditionalTests.js
@@ -1,11 +1,44 @@
 const internetPage = require('../pages/internet.page')
 
-describe('Traditional Tests', () => {
-  it('Login Page UI Elements: Is the logo image displayed', () => {
+describe('Test to ensure Login Page UI elements look OK', () => {
+  it('should detect if logo image is existing', () => {
     console.log(browser.url(`${browser.options.baseUrl}`));
-    expect(internetPage.logoImage.isDisplayed()).equals(true);
+    expect(internetPage.logoImage.isExisting()).equals(true, 'expected logo image to exist');
   });
-  it('Login Page UI Elements: Is the auth header displayed', () => {
-    expect(internetPage.authHeader.isDisplayed()).equals(true);
+  it('should detect if auth header is existing', () => {
+    expect(internetPage.authHeader.isExisting()).equals(true, 'expected auth header to exist');
   });
+  it('should get correct auth header text', () => {
+    expect(internetPage.authHeader.getText()).equals('Login Form', 'expected auth header text to be "Login Form"');
+  });
+  it('should detect if auth header css property is existing', () => {
+    // webdriver unable to test pseudo element "::after"
+  });
+  it('should detect if username form label is existing', () => {
+    expect(internetPage.formLabelUsername.isExisting()).equals(true, 'expected username form label to exist');
+  });
+  it('should get correct username form label text', () => {
+    expect(internetPage.formLabelUsername.getText()).equals('Username', 'expected username form label text to be "Username"');
+  });
+  it('should detect if username form input is existing', () => {
+    expect(internetPage.username.isExisting()).equals(true, 'expected username form input to exist');
+  });
+  it('should detect if username form placeholder is existing', () => {
+    expect(internetPage.username.getAttribute('placeholder')).equals('Enter your username', 'expected username form placeholder text to be "Enter your username"');
+  });
+  it('should detect if password form label is existing', () => {
+    expect(internetPage.formLabelPassword.isExisting()).equals(true, 'expected password form label to exist');
+  });
+  it('should get correct password form label text', () => {
+    expect(internetPage.formLabelPassword.getText()).equals('Password', 'expected password form label text to be "Password"');
+  });
+  it('should detect if password form input is existing', () => {
+    expect(internetPage.password.isExisting()).equals(true, 'expected password form input to exist');
+  });
+  it('should detect if password form placeholder is existing', () => {
+    expect(internetPage.password.getAttribute('placeholder')).equals('Enter your password', 'expected password form placeholder text to be "Enter your password"');
+  });
+
+  // TODO: write tests for all elements on page
+
 });
