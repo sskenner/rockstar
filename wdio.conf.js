9fc2304bf97189e46bc809c79bd35a36fbeec897
skA <sskenner@gmail.com>
2019-11-19 00:51:17 -0500

    update wdio.config > services to fix ERROR: connect ECONNREFUSED 127.0.0.1:4444; works as wdio4

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,8 +10,19 @@ if (!ENV || !['v1', 'v2'].includes(ENV)) {
     console.log('Please use the following format to run test scripts: ENV=v1|v2')
     process.exit()
 }
+
 exports.config = {
-    
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
+    // on a remote machine).
+    runner: 'local',
+    //
+    // Override default path ('/wd/hub') for chromedriver service.
+    path: '/',
     //
     // ==================
     // Specify Test Files
@@ -21,6 +32,9 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
+    // specs: [
+    //     './test/**/*.js',
+    // ],
     specs: [
         './test/specs/**/*.js'
     ],
@@ -56,9 +70,11 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        // browserName: 'chrome'
         browserName: 'chrome',
-        args: ["--start-maximized", "--start-fullscreen"],
+        // If outputDir is provided WebdriverIO can capture driver session logs
+        // it is possible to configure which logTypes to include/exclude.
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['bugreport', 'server'],
     }],
     //
     // ===================
@@ -66,32 +82,34 @@ exports.config = {
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
     //
-    // By default WebdriverIO commands are executed in a synchronous way using
-    // the wdio-sync package. If you still want to run your tests in an async way
-    // e.g. using promises you can set the sync option to false.
-    sync: true,
-    //
-    // Level of logging verbosity: silent | verbose | command | data | result | error
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    // logLevel: 'info',
     logLevel: 'silent',
     //
-    // Enables colors for log output.
-    coloredLogs: true,
-    //
-    // Warns when a deprecated command is used
-    deprecationWarnings: true,
+    // Set specific log levels per logger
+    // loggers:
+    // - webdriver, webdriverio
+    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
+    // - @wdio/mocha-framework, @wdio/jasmine-framework
+    // - @wdio/local-runner, @wdio/lambda-runner
+    // - @wdio/sumologic-reporter
+    // - @wdio/cli, @wdio/config, @wdio/sync, @wdio/utils
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    // logLevels: {
+    //     webdriver: 'info',
+    //     '@wdio/applitools-service': 'info'
+    // },
     //
     // If you only want to run your tests until a specific amount of tests have failed use
     // bail (default is 0 - don't bail, run all tests).
     bail: 0,
-    //
-    // Saves a screenshot to a given path if a command fails.
-    screenshotPath: './errorShots/',
     //
     // Set a base URL in order to shorten url command calls. If your `url` parameter starts
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
     baseUrl: url[process.env.ENV],
+    // baseUrl: 'https://demo.applitools.com/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -103,60 +121,45 @@ exports.config = {
     // Default request retries count
     connectionRetryCount: 3,
     //
-    // Initialize the browser instance with a WebdriverIO plugin. The object should have the
-    // plugin name as key and the desired plugin options as properties. Make sure you have
-    // the plugin installed before running any tests. The following plugins are currently
-    // available:
-    // WebdriverCSS: https://github.com/webdriverio/webdrivercss
-    // WebdriverRTC: https://github.com/webdriverio/webdriverrtc
-    // Browserevent: https://github.com/webdriverio/browserevent
-    // plugins: {
-    //     webdrivercss: {
-    //         screenshotRoot: 'my-shots',
-    //         failedComparisonsRoot: 'diffs',
-    //         misMatchTolerance: 0.05,
-    //         screenWidth: [320,480,640,1024]
-    //     },
-    //     webdriverrtc: {},
-    //     browserevent: {}
-    // },
-    //
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['chromedriver'],
-    services: [ 
-        ['image-comparison', 
-        // The options
-        {
-            // Some options, see the docs for more
-            baselineFolder: join(process.cwd(), './tests/sauceLabsBaseline/'),
-            formatImageName: '{tag}-{logName}-{width}x{height}',
-            screenshotPath: join(process.cwd(), '.tmp/'),
-            savePerInstance: true,
-            autoSaveBaseline: true,
-            blockOutStatusBar: true,
-            blockOutToolBar: true,
-            // ... more options
-        }], ['chromedriver'], ['applitools'],
-    ],
+    // services: ['chromedriver','selenium-standalone'],
+    services: ['chromedriver'],
+    // services: [ 
+    //     ['image-comparison', 
+    //     // The options
+    //     {
+    //         // Some options, see the docs for more
+    //         baselineFolder: join(process.cwd(), './tests/sauceLabsBaseline/'),
+    //         formatImageName: '{tag}-{logName}-{width}x{height}',
+    //         screenshotPath: join(process.cwd(), '.tmp/'),
+    //         savePerInstance: true,
+    //         autoSaveBaseline: true,
+    //         blockOutStatusBar: true,
+    //         blockOutToolBar: true,
+    //         // ... more options
+    //     }], ['chromedriver'], ['applitools'],
+    // ],
     // applitoolsKey: process.env.APPLITOOLS_KEY,    
-    applitoolsKey: process.env.APPLITOOLS_KEY,
-    //
+    applitoolsKey: process.env.APPLITOOLS_KEY,    
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: http://webdriver.io/guide/testrunner/frameworks.html
+    // see also: https://webdriver.io/docs/frameworks.html
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
     framework: 'mocha',
     //
+    // The number of times to retry the entire specfile when it fails as a whole
+    // specFileRetries: 1,
+    //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
-    // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['dot','spec'],
-    
+    // see also: https://webdriver.io/docs/dot-reporter.html
+    reporters: ['spec','dot'],
+ 
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -194,8 +197,6 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
     before: function (capabilities, specs) {
         browser.url(`${browser.options.baseUrl}`)
     },
@@ -206,19 +207,16 @@ exports.config = {
      */
     // beforeCommand: function (commandName, args) {
     // },
-    
     /**
      * Hook that gets executed before the suite starts
      * @param {Object} suite suite details
      */
     // beforeSuite: function (suite) {
+        
     // },
     /**
-     * Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-     * @param {Object} test test details
+     * Function to be executed before a test (in Mocha/Jasmine) starts.
      */
-    // beforeTest: function (test) {
-    // },
     beforeTest: function () {
         const chai = require('chai')
         const chaiWebdriver = require('chai-webdriverio').default
@@ -233,27 +231,27 @@ exports.config = {
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
      */
-    // beforeHook: function () {
+    // beforeHook: function (test, context) {
     // },
     /**
-     * Hook that gets executed _after_ a hook within the suite ends (e.g. runs after calling
+     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
      * afterEach in Mocha)
      */
-    // afterHook: function () {
+    // afterHook: function (test, context, { error, result, duration, passed, retries }) {
     // },
     /**
-     * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) ends.
-     * @param {Object} test test details
+     * Function to be executed after a test (in Mocha/Jasmine).
      */
-    // afterTest: function (test) {
+    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
     // },
+
+
     /**
      * Hook that gets executed after the suite has ended
      * @param {Object} suite suite details
      */
     // afterSuite: function (suite) {
     // },
-    
     /**
      * Runs after a WebdriverIO command gets executed
      * @param {String} commandName hook command name
@@ -281,11 +279,20 @@ exports.config = {
     // afterSession: function (config, capabilities, specs) {
     // },
     /**
-     * Gets executed after all workers got shut down and the process is about to exit.
+     * Gets executed after all workers got shut down and the process is about to exit. An error
+     * thrown in the onComplete hook will result in the test run failing.
      * @param {Object} exitCode 0 - success, 1 - fail
      * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities) {
-    // }
+    // onComplete: function(exitCode, config, capabilities, results) {
+    // },
+    /**
+    * Gets executed when a refresh happens.
+    * @param {String} oldSessionId session ID of the old session
+    * @param {String} newSessionId session ID of the new session
+    */
+    //onReload: function(oldSessionId, newSessionId) {
+    //}
 }
